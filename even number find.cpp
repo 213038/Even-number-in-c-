4a31974e77e8cbfa7ed8e47986ad66e47a7210c8
213038 <120672066+213038@users.noreply.github.com>
2022-12-15 22:21:23 +0500

    Add files via upload

---
+++ even number find.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<" Enter  number"<<endl;
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"even number is"<<" "<<a<<endl;
+	}
+	else
+	{
+				cout<<"odd number is"<<" "<<a<<endl;
+
+	}
+	
+}
